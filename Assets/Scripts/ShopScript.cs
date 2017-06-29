9196c5031d9b2dd0461eb7982774471ac1bc4467
Ebaad Imran <Ebaad Imran>
2017-06-29 15:26:42 -0500

    Updated Store

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -12,7 +12,19 @@ public class ShopScript : MonoBehaviour
     public Text Title;
     public Text ShopInfo;
     public Text TotalScoreTxt;
+    public Text RootsSelected;
+    public Text SpectreSelected;
+    public Text FadeSelected;
+    public Text LightSelected;
+    public AudioSource PlayRootsSouce;
+    public AudioSource PlaySpectreSource;
+    public AudioSource PlayFadeSource;
+    public AudioSource PlayLightSource;
     public float totalScore;
+    private float TobuRootsEnabled;
+    private float WalkerSpectreEnabled;
+    private float WalkerFadeEnabled;
+    private float ElectroLightEnabled;
 
     void Start()
     {
@@ -21,7 +33,24 @@ public class ShopScript : MonoBehaviour
         Title = Title.GetComponent<Text>();
         MusicShop = MusicShop.GetComponent<Canvas>();
         ColorShop = ColorShop.GetComponent<Canvas>();
+        MusicShop.enabled = false;
+        ColorShop.enabled = false;
         totalScore = PlayerPrefs.GetFloat("TotalScore");
+        SetMusicToSave();
+        if(WalkerSpectreEnabled == 0 && WalkerFadeEnabled == 0 && ElectroLightEnabled == 0)
+        {
+            SelectTobu();
+
+        }else if(WalkerSpectreEnabled == 1)
+        {
+            SelectSpectre();
+        }else if(WalkerFadeEnabled == 1)
+        {
+            SelectFade();
+        }else
+        {
+
+        }
         UpdateText();
     }
 
@@ -37,6 +66,7 @@ public class ShopScript : MonoBehaviour
 
     public void Back()
     {
+        SetMusicSaves();
         SceneManager.LoadScene("Start");
     }
 
@@ -59,5 +89,75 @@ public class ShopScript : MonoBehaviour
         ShopInfo.enabled = false;
         MusicShop.enabled = false;
         ColorShop.enabled = false;
+    }
+
+    public void SelectTobu()
+    {
+        DeselectMusic();
+        TobuRootsEnabled = 1;
+        RootsSelected.enabled = true;
+    }
+
+    public void PlayRoots()
+    {
+        StopAllMusic();
+        PlayRootsSouce.Play();
+    }
+
+    public void StopAllMusic()
+    {
+        PlayRootsSouce.Stop();
+        PlaySpectreSource.Stop();
+        PlayFadeSource.Stop();
+        PlayLightSource.Stop();
+    }
+
+    public void SelectSpectre()
+    {
+        DeselectMusic();
+        WalkerSpectreEnabled = 1;
+        SpectreSelected.enabled = true;
+    }
+
+    public void SelectFade()
+    {
+        DeselectMusic();
+        WalkerFadeEnabled = 1;
+        FadeSelected.enabled = true;
+    }
+
+    public void SelectLight()
+    {
+        DeselectMusic();
+        ElectroLightEnabled = 1;
+        LightSelected.enabled = true;
+    }
+
+    public void DeselectMusic()
+    {
+        TobuRootsEnabled = 0;
+        WalkerFadeEnabled = 0;
+        WalkerSpectreEnabled = 0;
+        ElectroLightEnabled = 0;
+        RootsSelected.enabled = false;
+        //FadeSelected.enabled = false;
+        //SpectreSelected.enabled = false;
+        //LightSelected.enabled = false;
+    }
+
+    public void SetMusicToSave()
+    {
+        TobuRootsEnabled = PlayerPrefs.GetFloat("RootsEnabled");
+        WalkerSpectreEnabled = PlayerPrefs.GetFloat("SpectreEnabled");
+        WalkerFadeEnabled = PlayerPrefs.GetFloat("FadeEnabled");
+        ElectroLightEnabled = PlayerPrefs.GetFloat("LightEnabled");
+    }
+
+    public void SetMusicSaves()
+    {
+        PlayerPrefs.SetFloat("RootsEnabled", TobuRootsEnabled);
+        PlayerPrefs.SetFloat("SpectreEnabled", WalkerSpectreEnabled);
+        PlayerPrefs.SetFloat("FadeEnabled", WalkerFadeEnabled);
+        PlayerPrefs.SetFloat("LightEnabled", ElectroLightEnabled);
     }
 }
